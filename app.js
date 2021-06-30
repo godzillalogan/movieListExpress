31311b7a7b42ce64baaf9e28f27db25ad3e3efa3
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-06-30 20:29:15 +0800

    2021.6.30, HOME, S3 U63 打造 Show 頁面

--- app.js
+++ app.js
@@ -15,42 +15,6 @@ app.use(express.static('public'))
 
 //routes setting
 app.get('/',(req,res) =>{
-  // create a variable to store movieOne
-  // const movieList = [
-  //   {
-  //     id: 1,
-  //     title: 'Jurassic World: Fallen Kingdom',
-  //     image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg',
-  //   }, {
-  //     id: 2,
-  //     title: 'Ant-Man and the Wasp',
-  //     image: 'https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg',
-  //   },{
-  //     id: 3,
-  //     title: 'Thor: Ragnarok',
-  //     image: 'https://movie-list.alphacamp.io/posters/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg',
-  //   },{
-  //     id: 4,
-  //     title: 'Avengers: Infinity War',
-  //     image: 'https://movie-list.alphacamp.io/posters/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg',
-  //   },{
-  //     id: 5,
-  //     title: 'Mission: Impossible - Fallout',
-  //     image: 'https://movie-list.alphacamp.io/posters/80PWnSTkygi3QWWmJ3hrAwqvLnO.jpg',
-  //   },{
-  //     id: 6,
-  //     title: 'Incredibles 2',
-  //     image: 'https://movie-list.alphacamp.io/posters/x1txcDXkcM65gl7w20PwYSxAYah.jpg',
-  //   },{
-  //     id: 7,
-  //     title: 'Fifty Shades Freed',
-  //     image: 'https://movie-list.alphacamp.io/posters/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg',
-  //   },{
-  //     id: 8,
-  //     title: 'The First Purge',
-  //     image: 'https://movie-list.alphacamp.io/posters/2slvblTroiT1lY9bYLK7Amigo1k.jpg',
-  //   }
-  // ]
 
   const numberList = [1, 2, 3, 4, 5, 6, 7, 8]
 
@@ -58,6 +22,17 @@ app.get('/',(req,res) =>{
   
 })
 
+app.get('/movies/1',(req,res) => {
+  const movieOne = {
+    title: 'Jurassic World: Fallen Kingdom',
+    image:  'c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg',
+    release_data: '2018-06-06 ',
+    description: `
+      Several years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar. Claire Dearing, the former park manager and founder of the Dinosaur Protection Group,recruits Owen Grady to help prevent the extinction of the dinosaurs once again.
+    `
+  }
+  res.render('show', {movie : movieOne})
+})
 
 //start and listen on the Express server
 app.listen(port, () =>{
